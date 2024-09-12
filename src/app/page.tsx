91956f890c136a2da7e90ae3e1393a692dc13c24
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-09-12 23:32:59 +0530

    Fixed Stock Context Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,9 @@ import StockForm from "./components/StockForm";
 export default function Home() {
   return (
     <>
-
-
-
     <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
     <StockForm />
     </div>
-
-
-    
-
-
-
     </>
   );
 }
